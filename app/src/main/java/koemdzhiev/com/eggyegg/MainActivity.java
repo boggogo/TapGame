222df1dd56182f99f1bc515f2ae9fcd82de7d623
boggogo <koemdjiev@gmail.com>
2015-06-14 23:26:49 +0100

    vibration added animation to the tapImage and mTitle

--- app/src/main/java/koemdzhiev/com/eggyegg/MainActivity.java
+++ app/src/main/java/koemdzhiev/com/eggyegg/MainActivity.java
@@ -3,6 +3,7 @@ package koemdzhiev.com.eggyegg;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -26,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView countTap;
     SharedPreferences mSharedPreferences;
     SharedPreferences.Editor mEditor;
+    Vibrator mVibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        // countTap = (TextView)findViewById(R.id.countTap);
-
+        mVibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         tapImage = (ImageView)findViewById(R.id.tapImage);
@@ -53,11 +55,10 @@ public class MainActivity extends AppCompatActivity {
         });
         mSharedPreferences = getPreferences(Context.MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
-        i = mSharedPreferences.getInt(MyConstants.KEY_TAB_NUMBER, 1000000);
+        i = mSharedPreferences.getInt(MyConstants.KEY_TAB_NUMBER, 100);
         mTitle = (TextView) mToolbar.findViewById(R.id.toolbar_title);
         mTitle.setText(i + "");
         mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 65);
-
         mTitle.setTextColor(getResources().getColor(R.color.text_white_color));
         setSupportActionBar(mToolbar);
         if (getSupportActionBar()!=null) {
@@ -69,20 +70,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setEggImage() {
-        if(i < 840000){
+        //840000
+        if(i <= 80){
             tapImage.setImageResource(R.drawable.egg2);
+            if(i == 80){
+                mVibrator.vibrate(400);
+                YoYo.with(Techniques.Swing).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.FadeIn).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.Swing).duration(800).playOn(tapImage);
+            }
         }
-        if(i < 680000){
+        //680000
+        if(i <= 60){
             tapImage.setImageResource(R.drawable.egg3);
+            if(i == 60){
+                mVibrator.vibrate(400);
+                YoYo.with(Techniques.Swing).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.FadeIn).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.Swing).duration(800).playOn(tapImage);
+            }
         }
-        if(i < 520000){
+        //520000
+        if(i <= 40){
             tapImage.setImageResource(R.drawable.egg4);
+            if(i == 40){
+                mVibrator.vibrate(400);
+                YoYo.with(Techniques.FadeOut).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.FadeIn).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.Swing).duration(800).playOn(tapImage);
+            }
         }
-        if(i < 360000){
+        //360000
+        if(i <= 20){
             tapImage.setImageResource(R.drawable.egg5);
+            if(i == 20){
+                mVibrator.vibrate(400);
+                YoYo.with(Techniques.FadeOut).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.FadeIn).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.Swing).duration(800).playOn(tapImage);
+            }
         }
-        if(i < 200000){
+        //200000
+        if(i <= 10){
             tapImage.setImageResource(R.drawable.egg6);
+            if(i == 10){
+                mVibrator.vibrate(400);
+                YoYo.with(Techniques.FadeOut).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.FadeIn).duration(300).playOn(mTitle);
+                YoYo.with(Techniques.Swing).duration(800).playOn(tapImage);
+            }
         }
     }
 
