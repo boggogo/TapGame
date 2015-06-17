83f9ebbbaf0c3bd63c111aeeba8300a9a03cae3b
boggogo <koemdjiev@gmail.com>
2015-06-17 19:49:47 +0100

    added onClickListener to the contributor's relayout

--- app/src/main/java/koemdzhiev/com/eggyegg/ContributorsActivity.java
+++ app/src/main/java/koemdzhiev/com/eggyegg/ContributorsActivity.java
@@ -2,14 +2,25 @@ package koemdzhiev.com.eggyegg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
 public class ContributorsActivity extends AppCompatActivity {
+    private RelativeLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contributors);
+
+        mLayout = (RelativeLayout)findViewById(R.id.contributorsRelativeLayout);
+        mLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
